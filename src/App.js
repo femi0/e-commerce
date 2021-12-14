31772da12ba7b54d1df6273b03402729c4df74c0
femi001 <femo.arek@gmail.com>
2021-12-14 08:55:23 -1200

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,12 +31,8 @@ class App extends React.Component {
             id: snapShot.id,
             ...snapShot.data()
           }
-        },
-        ()=> {
-           console.log(this.state);
         });
-
-
+        
         });
     }
 
--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 
 import './sign-in.styles.scss';
@@ -19,10 +19,20 @@ class SignIn extends React.Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState ({email:'', password:''})
-    }
+
+        const { email, password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});
+
+        }catch(error){
+            console.log(error);
+        }
+
+    };
 
     handleChange = event => {
         const {value, name} = event.target;
@@ -49,9 +59,9 @@ class SignIn extends React.Component{
                     <FormInput
                     name="password" 
                     type="password" 
-                    value={this.state.email}
+                    value={this.state.password}
                     handleChange={this.handleChange}
-                    label='label'
+                    label='Password'
                     required/>
                      
                     
